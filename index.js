e8dd9d4c4735543d0d109194dacf9da7933c044b
Aden Cronje <adencronje05@gmail.com>
2024-06-07 11:41:34 +0200

    Styled text a bit and changed up the dogecoin emoji's

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     ).innerHTML = `<img src = ${data.image.small}/>
     <span>${data.name}</span>`;
     document.getElementById("crypto").innerHTML += `
-    <p>🎯: R${data.market_data.current_price.zar}</p>
-    <p>👆: R${data.market_data.high_24h.zar}</p>
-    <p>👇: R${data.market_data.low_24h.zar}</p>
+    <p>🏆: R${data.market_data.current_price.zar}</p>
+    <p>⬆️: R${data.market_data.high_24h.zar}</p>
+    <p>⬇️: R${data.market_data.low_24h.zar}</p>
 `;
   })
   .catch((err) => console.error(err));
